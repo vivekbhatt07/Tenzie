d92d604ac5a1616826d0c287f6b498333696dcf3
Vivek Bhatt <vivekbhatt618@gmail.com>
2022-11-18 10:00:47 +0530

    Add: Die & Die Container

---
+++ src/Components/Die Container/DieContainer.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from "react";
+
+function DieContainer() {
+  return (
+    <Fragment>
+      <div className="die_container"></div>
+    </Fragment>
+  );
+}
+
+export default DieContainer;
