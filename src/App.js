2f1c17594dae33eb74526f08bba20898841bcfb9
Vivek Bhatt <vivekbhatt618@gmail.com>
2022-11-18 10:14:32 +0530

    Add: random Number Functionality

--- src/App.js
+++ src/App.js
@@ -2,6 +2,15 @@ import React from "react";
 import "./App.css";
 import DieContainer from "./Components/Die Container/DieContainer";
 function App() {
+  // Generates Random Number Between 1-6 :
+  function randomNumberGenerator() {
+    return Math.ceil(Math.random() * 6);
+  }
+  randomNumberGenerator();
+
+  // Loop that generates an array with 10 Random Numbers:
+  for (let i = 0; i < 10; i++) {}
+
   return (
     <div className="App">
       <DieContainer />
